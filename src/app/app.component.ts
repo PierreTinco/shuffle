cf5161e9fbe310fcd9b2444dd19cf7cb4cd59bf4
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-01-11 18:52:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  // Web3 = require('web3');
+  // web3 = new this.Web3('HTTP://127.0.0.1:7545');
+  
+  
+  constructor() {
+    // console.log('ok');
+  }
 }
