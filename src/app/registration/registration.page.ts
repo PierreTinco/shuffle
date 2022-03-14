9be547b30fd3a9c74d90832a7b422a5b1a404c04
williamprevot <guy-william.prevot@isen.yncrea.fr>
2022-03-14 19:23:16 +0100

    registration bouton deplacement

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import '@firebase/auth'
+import { environment } from 'src/environments/environment';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
-
+  app: any
+  analytics: any
+  user = {
+    gender: null,
+    name: "",
+    surname : "",
+    birth_date :"",
+    email: "",
+    password : "",
+    wallet : "",
+    phone_number : ""
+  }
+  auth: any
+  email:any;
+  password: any;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {this.app = initializeApp(environment.firebaseConfig);
+    this.analytics = getAnalytics(this.app);
+    this.auth = getAuth()
+  }
+  register(email: any, password: any) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      
+      
+      // ..
+    });
   }
 
 }
