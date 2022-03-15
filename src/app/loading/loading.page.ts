77d4f622e4c252718f4c5ecb26e0ccc3cd2ad9e4
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-03-15 15:55:54 +0100

    loading update

--- src/app/loading/loading.page.ts
+++ src/app/loading/loading.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import{ Router } from '@angular/router'
+import { url } from 'inspector';
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.page.html',
   styleUrls: ['./loading.page.scss'],
 })
 export class LoadingPage implements OnInit {
-
+  logo: any
   constructor(private routes:Router) { }
 
   ngOnInit() {
+    this.logo = '..\..\logo.png'
     setTimeout(() => {
       this.routes.navigateByUrl('welcome');
     }, 3000);
