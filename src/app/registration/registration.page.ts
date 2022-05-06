23fff149babfa61e176d0f0e2e20f77a3f272981
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-05-06 19:08:02 +0200

    test form

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Injectable, OnInit, Output } from '@angular/core';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { ApiService } from '../services/api.service';
 import{ Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, NgForm} from "@angular/forms";
@@ -13,23 +13,12 @@ export class RegistrationPage implements OnInit {
   app: any
   analytics: any
   submitted = false;
-  user = {
-    gender: null,
-    name: "",
-    surname : "",
-    birth_date :"",
-    wallet : "",
-    phone_number : "",
-    token : ""
-  }
   auth: any
-  email:any;
-  password: any;
   //repeatPassword:any;
  
   showPassword = false;
   public passwordToggleIcon = "eye-off";
-  private myForm: FormGroup;
+  public myForm: FormGroup;
   public age: number;
   constructor(private api: ApiService, private router:Router,private fb: FormBuilder) {
 
@@ -43,43 +32,39 @@ export class RegistrationPage implements OnInit {
     this.myForm.valueChanges.subscribe(data => console.log('form changes', data));
     this.myForm.valueChanges.subscribe(el => {
      console.log( 'my Form validity',this.myForm.valid); })
-    // this.myForm.controls.valid
-    
   }
   
   initForm(): void{
-    this.myForm=  this.fb.group ({
+    this.myForm =  this.fb.group ({
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!?.,:;"@#$%^£&*])(?=.{6,})'), Validators.minLength(6)]],
-      repeatPassword: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!?.,:;"@#$%^£&*])(?=.{6,})'), Validators.minLength(6)]],
-      //  user: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', [Validators.required, Validators.minLength(6)]],
+        user : {
         gender: ['', [Validators.required]],
         name: ['',[Validators.required, Validators.minLength(3)]],
         surname : ['',[Validators.required, Validators.minLength(3)]],
         birth_date :['',[Validators.required]],
         phone_number : ['', [Validators.required, Validators.minLength(9), Validators.pattern('^[0-9]{10}$')]],
-        //  })
+        }
        });   
   }
 
 
-  validForm(){
+    validForm(){
+    console.log( 'my Form validity',this.myForm.valid)
     this.submitted = true;
  
-    // if (!this.myForm.valid) {
-    //   console.log('All fields are required.')
-    //   alert('Please provide all the required fields.')
+    if (!this.myForm.valid) {
+      console.log('All fields are required.')
+      alert('Please provide all the required fields.')
       
-    //   return false;
-    //} else {
+      return false;
+    } else {
       console.log(this.myForm.value) 
-      this.register(this.email, this.password)
-      
-      
-   // }
+      this.register(this.myForm.value.email, this.myForm.value.password)
+      }
    }
   async register(email: any, password: any) {
-    console.log(this.email);
     await createUserWithEmailAndPassword(this.auth, email, password)
     .then((userCredential) => {
       // Signed in 
@@ -122,10 +107,10 @@ export class RegistrationPage implements OnInit {
 
   }
   async addUserToDb(token: any) {
-    this.user.token = token
-    console.log(this.user);
+    this.myForm.value.user.token = token
+
     
-    await this.api.addUser(this.user).subscribe(
+    await this.api.addUser(this.myForm.value.user).subscribe(
       (res) => {
         alert("vos avez bien créé votre compte");
       },
